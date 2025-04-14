2dfd37d48f8d8d4a1b7f40d02624cb1af92fee29
Himanshu <himanshukasoudhan5421@gmail.com>
2025-04-14 22:51:50 +0530

    Few changes

--- notes/vite.config.js
+++ notes/vite.config.js
@@ -5,6 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Ensure assets are loaded from root
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
